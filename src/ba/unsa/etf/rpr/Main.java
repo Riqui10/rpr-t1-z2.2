5545036a443bab5b537079a705090071c8bbcc0d
Riqui10 <rzejnilovi1@etf.unsa.ba>
2020-10-30 13:27:49 +0100

    Commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara(int n){
+        int suma=0;
+
+        while(n>0){
+            int broj = n%10;
+            suma = suma + broj;
+            n=n/10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int n;
+	    n = in.nextInt();
+
+	    for(int i=1;i<=n;i++){
+	        if(i%sumaCifara(i)==0)
+                System.out.println(i);
+        }
+    }
+}
